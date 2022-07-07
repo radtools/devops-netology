0c33babb5b4e1ed5016dcbc04308eb7118e88cae
radtools <93760545+radtools@users.noreply.github.com>
2022-07-07 14:21:57 +0500

    Create main.tf

---
+++ virt-homeworks/07-terraform-03-basic/files/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "<имя бакета>"
+    region     = "ru-central1"
+    key        = "<путь к файлу состояния в бакете>/<имя файла состояния>.tfstate"
+    access_key = "<идентификатор статического ключа>"
+    secret_key = "<секретный ключ>"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
