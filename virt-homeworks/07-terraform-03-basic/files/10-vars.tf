21a00cf8c2800e01965ed34b7a9e4cb64c8c724b
radtools <93760545+radtools@users.noreply.github.com>
2022-07-08 16:38:48 +0500

    Update 10-vars.tf

--- virt-homeworks/07-terraform-03-basic/files/10-vars.tf
+++ virt-homeworks/07-terraform-03-basic/files/10-vars.tf
@@ -1,17 +1,19 @@
 #переменные
-
+###########
+#начинаются тут
 variable "zone" {
   default = "ru-central1-b"
 }
 #oAuth_vars
+
 variable "yandex-token" {
   default = "zzzzzzz"
 }
-
+#cloud_id_var
 variable "yandex-cloud-id" {
   default = "1234567"
 }
-
+#folder_id_var
 variable "yandex-folder-id" {
   default = "xyzxyzxyz"
 }
@@ -24,6 +26,7 @@ variable "instance_additional_disk" {
   default = "2"
 }
 #VM_name_vars
+
 variable "vm-name" {
   default = "debian-srv"
 }
@@ -32,11 +35,14 @@ variable "discpiption" {
   default = "my_Debian_test_srv"
 }
 #Image_ID_yandexMarket
+
 variable "image-id" {
   default = "fd86ul0ci8ttbst35ln3"
 }
 
 #bucket_vars
+#
+#
 variable "buc_name" {
   default = "ya-buck"
 }
