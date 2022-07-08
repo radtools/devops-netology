0414acad6bc7f1b9ef18edde8d1872af7aeae0b0
radtools <93760545+radtools@users.noreply.github.com>
2022-07-08 16:14:17 +0500

    Delete virt-homeworks/07-terraform-02-syntax/file directory

--- virt-homeworks/07-terraform-02-syntax/file/output.tf
+++
@@ -1,7 +0,0 @@
-output "internal_ip_address_ubuntu" {
-  value = "${yandex_compute_instance.vm-Ubuntu-srv.network_interface.0.ip_address}"
-}
-
-output "external_ip_address_centos7" {
-  value = "${yandex_compute_instance.vm-Ubuntu-srv.network_interface.0.nat_ip_address}"
-}
