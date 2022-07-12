455c84b4120a68e473760d424705fe4613597283
radtools <93760545+radtools@users.noreply.github.com>
2022-07-12 15:53:11 +0500

    Delete 05-output.tf

--- virt-homeworks/07-terraform-03-basic/file/count/05-output.tf
+++
@@ -1,7 +0,0 @@
-output "internal_ip_address_srv" {
-  value = yandex_compute_instance.vmstudy-07-03[0].network_interface.0.ip_address
-}
-
-output "external_ip_address_srv" {
-  value = yandex_compute_instance.vmstudy-07-03[0].network_interface.0.nat_ip_address
-}
