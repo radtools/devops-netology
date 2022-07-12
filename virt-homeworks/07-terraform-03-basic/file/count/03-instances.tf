21abfe50f23486da2fe84ff6449a8fd6246ce46a
radtools <93760545+radtools@users.noreply.github.com>
2022-07-12 15:52:22 +0500

    Update 03-instances.tf

--- virt-homeworks/07-terraform-03-basic/file/count/03-instances.tf
+++ virt-homeworks/07-terraform-03-basic/file/count/03-instances.tf
@@ -1,19 +1,13 @@
-locals {
-  instance_name = "${terraform.workspace}-instance[0]"
-}
-
-
-
 resource "yandex_compute_instance" "vmstudy-07-03"{
-  count = var.instance_count
-  name = local.instance_name
-  description = "discpiption - ${terraform.workspace}" 
+  count                     = var.instance_count
+  name                      = "vm-${count.index}-${terraform.workspace}"
+  description               = "discpiption - ${terraform.workspace}" 
   allow_stopping_for_update = true
-  platform_id = "standard-v2"
+  platform_id               = var.instance_type
   resources {
-    cores         = var.cores
-    memory        = var.mem
-    core_fraction = var.fraction
+    cores                   = var.cores
+    memory                  = var.mem
+    core_fraction           = var.fraction
   }
 
 
@@ -23,25 +17,14 @@ resource "yandex_compute_instance" "vmstudy-07-03"{
     initialize_params {
       # Ubuntu 20.04
       image_id    = var.image-id                                    #переменная ID образа из маркетплейса ЯО
-      name        = "disk-${terraform.workspace}"                   #Имя диска  
+      name        = "vm${count.index}-disk-${terraform.workspace}"                   #Имя диска  
       description = "Disk for the root - ${terraform.workspace}"    #Описание диска
       size        = var.instance_root_disk                          #размер указан в переменных
     }
   }
 
-  secondary_disk {
-    disk_id     = yandex_compute_disk.disk-srv[0].id
-    auto_delete = true
-  }
-
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet[0].id
+    subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
     }
-}
-resource "yandex_compute_disk" "disk-srv" {
-  count = var.instance_count
-  name = "disk-srv-[0]${terraform.workspace}"
-  type = "network-hdd"
-  size = var.instance_additional_disk                                 #размер указан в переменных
 }
