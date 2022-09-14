a93e04a9d5e8911251526a6b5a98f9fe91c68e79
radtools <93760545+radtools@users.noreply.github.com>
2022-09-14 10:31:33 +0500

    Create variables.tf

---
+++ dev-ops-diploma/files/s3/variables.tf
@@ -0,0 +1,10 @@
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gp79eqhedrffiqjlhk"
+}
+
+
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1grkimcimnco8bilkh9"
+}
