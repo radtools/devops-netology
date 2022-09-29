d62df50dc2726500ce357ef44c647a0b790293c1
radtools <93760545+radtools@users.noreply.github.com>
2022-09-29 14:00:43 +0500

    Update output.tf

--- dev-ops-diploma/files/stage/output.tf
+++ dev-ops-diploma/files/stage/output.tf
@@ -5,3 +5,18 @@ output "internal_ip_address_proxy_yandex_cloud" {
 output "external_ip_address_proxy_yandex_cloud" {
   value = "${yandex_compute_instance.proxy.network_interface.0.nat_ip_address}"
 }
+output "internal_ip_address_db01_yandex_cloud" {
+  value = "${yandex_compute_instance.db01.network_interface.0.ip_address}"
+}
+output "internal_ip_address_db02_yandex_cloud" {
+  value = "${yandex_compute_instance.db02.network_interface.0.ip_address}"
+}
+output "internal_ip_address_wordpress_yandex_cloud" {
+  value = "${yandex_compute_instance.wp.network_interface.0.ip_address}"
+}
+output "internal_ip_address_Gitlab_yandex_cloud" {
+  value = "${yandex_compute_instance.git.network_interface.0.ip_address}"
+}
+output "internal_ip_address_monitoring_stack_yandex_cloud" {
+  value = "${yandex_compute_instance.mon.network_interface.0.ip_address}"
+}
